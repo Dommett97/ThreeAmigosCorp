f6384121d447109bb7d57fe7c3224b8e00f92f71
Dommett97 <callumdommett1@gmail.com>
2023-01-10 04:04:16 +0000

    Reducer and redux setup

--- frontend/src/screens/CartScreen.js
+++ frontend/src/screens/CartScreen.js
@@ -1,22 +1,22 @@
-import React, { useEffect } from "react"
-import { useDispatch, useSelector } from "react-redux"
-import { Row, Col, ListGroup, Image, Form, Button, Card } from 'react-bootstrap'
-import Message from '../components/Message'
-import { addToCart } from "../actions/cartActions"
+import React, {useEffect} from 'react'
+import { Row, Col, ListGroup, Image, Form, Button, Card} from 'react-bootstrap'
+import { useDispatch } from 'react-redux'
+import { addToCart } from '../actions/cartActions'
 
-const CartScreen = ({ match, location, history}) => {
-    const productId = match.params.id
+const CartScreen = ({ match, location, history }) => {
+    const productID = match.params.id
 
-    const qty = location.search ? location.search.split('=')[1] : 1
-    
+    const qty = location.search ? Number(location.search.split('=')[1]) : 1
+
     const dispatch = useDispatch()
 
     useEffect(() => {
-        if(productId){
-            dispatch(addToCart(productId, qty))
+        if (productID) {
+            dispatch(addToCart(productID, qty))
         }
-    }, [dispatch, productId, qty])
-    return <div>Cart</div>
+    }, [dispatch, productID, qty])
+
+    return<div>Cart</div>
 }
 
 export default CartScreen
