e95146586a66882823f2dab15bcc110f4aa9613e
Dommett97 <callumdommett1@gmail.com>
2023-01-13 17:08:56 +0000

    product reducer setup

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -10,7 +10,7 @@ import { listProducts } from '../actions/productActions'
 const HomeScreen = () => {
   const dispatch = useDispatch()
 
-  const productList =useSelector(state => state.productList)
+  const productList = useSelector((state) => state.productList)
   const { loading, error, products} = productList
 
   useEffect(() => {
@@ -27,7 +27,7 @@ const HomeScreen = () => {
     ) : (
     <Row>
         {products.map(product => (
-            <Col sm={12} md={6} lg={4} xl={3}>
+            <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
                 <Product product={product}/>
             </Col>
             ))}
