37d3738a3414c9d85dc5bbcf2ea916c9ba911ee3
Dommett97 <callumdommett1@gmail.com>
2023-01-16 12:57:58 +0000

    Creation of controllers, routes, and middleware

--- backend/server.js
+++ backend/server.js
@@ -3,17 +3,21 @@ import dotenv from "dotenv"
 import connectDB from './config/db.js'
 
 import productRoutes from './routes/productRoutes.js'
+import userRoutes from './routes/userRoutes.js'
 dotenv.config()
 
 connectDB()
 
 const app = express()
 
+app.use(express.json())
+
 app.get('/', (req, res) => {
     res.send('API is running...')
 })
 
 app.use('/api/products', productRoutes)
+app.use('/api/users', userRoutes)
 
 app.get('/api/products', (req, res) => {
     res.json(products)
